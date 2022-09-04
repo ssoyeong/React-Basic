57dc59ab4796ffe20390fa415eacdda1a18f26a3
ssoyeong <jsy171771@gmail.com>
2022-09-04 16:52:55 +0900

    React Render HTML

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const myFirstElement = <h1>Hello React!</h1>
+
+const mySecondElement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>Soyeong</td>
+    </tr>
+    <tr>
+      <td>Taylor</td>
+    </tr>
+  </table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(mySecondElement);
