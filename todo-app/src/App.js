40ee7629ca5d1b187101ce73a67b151c8e6ac774
ssoyeong <jsy171771@gmail.com>
2022-09-07 22:43:55 +0900

    Feat: TodoInsert 생성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
 
   return (
     <TodoTemplate>
-
+      <TodoInsert onInsert={onInsert} />
     </TodoTemplate>
   );
 };
