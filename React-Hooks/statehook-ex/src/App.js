d5495f7f495835c5bec48bcb67af48c5e28b5fdc
ssoyeong <jsy171771@gmail.com>
2022-09-11 18:00:19 +0900

    React-Hooks: useState with text input

--- React-Hooks/statehook-ex/src/App.js
+++ React-Hooks/statehook-ex/src/App.js
@@ -1,23 +1,37 @@
 import { useState } from 'react';
 
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!!");
+  return ['전소영', '소영이'];
+}
+
 function App() {
 
-  const [ time, setTime ] = useState(1);
-  const handleClick = () => {
-
-    let newTime;
-    if(time >= 12) newTime = 1;
-    else newTime = time + 1;
-
-    setTime(newTime);
+  const [names, setNames] = useState(() => {
+    return heavyWork();   // 초기값을 가져올 때 무거운 일인 경우 callback 형태로. 맨 처음 렌더링될 때만 콜
+  });
+  const [input, setInput] = useState("");
+  
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   }
 
-  console.log("업데이트!!");
+  const handleUpload = () => {
+    setNames((prevState) => {    // 업데이트 전 이전 상태
+      console.log("이전 " + prevState);
+      return([input, ...prevState]);
+    })
+  }
+
+  console.log(input);
 
   return (
     <div>
-      <span>현재 시각: { time }</span>
-      <button onClick={ handleClick }>업데이트</button>
+      <input type="text" value={input} onChange={handleInputChange}/>
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
