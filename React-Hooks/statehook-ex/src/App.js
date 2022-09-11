6700608e34624230205c5584545c0de752d5880b
ssoyeong <jsy171771@gmail.com>
2022-09-11 17:37:09 +0900

    React-Hooks: useState with button

--- React-Hooks/statehook-ex/src/App.js
+++ React-Hooks/statehook-ex/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 function App() {
+
+  const [ time, setTime ] = useState(1);
+  const handleClick = () => {
+
+    let newTime;
+    if(time >= 12) newTime = 1;
+    else newTime = time + 1;
+
+    setTime(newTime);
+  }
+
+  console.log("업데이트!!");
+
   return (
     <div>
-      state hook exercise
+      <span>현재 시각: { time }</span>
+      <button onClick={ handleClick }>업데이트</button>
     </div>
   );
 }
