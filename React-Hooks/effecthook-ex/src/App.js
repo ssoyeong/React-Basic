ad9f1b0c3c669abab2b4bdfca194369b408403dd
ssoyeong <jsy171771@gmail.com>
2022-09-12 19:16:17 +0900

    React-Hooks: useEffect 익히기

--- React-Hooks/effecthook-ex/src/App.js
+++ React-Hooks/effecthook-ex/src/App.js
@@ -1,18 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
   
   const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
   
   const handleCountUpdate = () => {
     setCount(count + 1);
-  }
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  // 렌더링될 때마다 실행
+  useEffect(() => {
+    console.log("rendering");
+  }, [count]);
+
+  // 마운트 될 때 && count가 업데이트 됐을 때 실행
+  useEffect(() => {
+    console.log("rendering -> count");
+  }, [count]);
+
+  // 마운트 될 때 && name이 업데이트 됐을 때 실행
+  useEffect(() => {
+    console.log("rendering -> name");
+  }, [name]);
+
+  // 마운트 될 때만, 맨 처음 렌더링될 때만 실행
+  useEffect(() => {
+    console.log("rendering -> first");
+  }, []);
 
   return (
     <div>
-      <button onClick={ handleCountUpdate}>Update</button>
+      <button onClick={ handleCountUpdate }>Update</button>
       <span>count: { count }</span>
+      <input type="text" value={ name } onChange={ handleInputChange }/>
+      <span>name: { name }</span>
     </div>
   );
 }
