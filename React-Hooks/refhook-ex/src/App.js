57439ebded10c1f780e1863da770fb99f0bace88
ssoyeong <jsy171771@gmail.com>
2022-09-13 17:36:36 +0900

    React-Hooks: useRef 렌더링 횟수 세기

--- React-Hooks/refhook-ex/src/App.js
+++ React-Hooks/refhook-ex/src/App.js
@@ -1,41 +1,19 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 const App = () => {
 
-  const [renderer, setRenderer] = useState(0);
-  const countRef = useRef(0);
-  let countVar = 0;
+  const [count, setCount] = useState(1);
+  const renderCount = useRef(0);
 
-  const doRendering = () => {
-    setRenderer(renderer + 1);
-  }
-
-  const increaseRef = () => {
-    countRef.current = countRef.current + 1;
-    console.log("ref: ", countRef.current);
-  };
-
-
-  const increaseVar = () => {
-    countVar = countVar + 1;
-    console.log("var: ", countVar);
-  };
-
-  const printResults = () => {
-    console.log(`ref: ${countRef.current}, var: ${countVar}`);
-  };
-
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;  // ref는 rerendering을 발생시키지 않음
+    console.log("렌더링 수: ", renderCount.current);
+  });
 
   return (
     <div>
-      <p>Ref: {countRef.current}</p>
-      <p>Var: {countVar}</p>
-      <button onClick={doRendering}>렌더😆</button>
-      <button onClick={increaseRef}>Ref 올려</button>
-      {/* ref 는 렌더 이전의 값을 가지고 있음 */}
-      <button onClick={increaseVar}>Var 올려</button>
-      {/* var는 렌더링 될 때마다 0으로 초기화됨 */}
-      <button onClick={printResults}>Ref Var 값 출력</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>올려주세용</button>
     </div>
   );
 };
