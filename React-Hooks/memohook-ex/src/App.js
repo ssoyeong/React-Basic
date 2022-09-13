5a445f72267218c85b74798c6277a0408d41ffe8
ssoyeong <jsy171771@gmail.com>
2022-09-13 21:25:26 +0900

    React-Hooks: useMemo 계산기 예제

--- React-Hooks/memohook-ex/src/App.js
+++ React-Hooks/memohook-ex/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const hardCalculate = (number)  => {
   console.log("어려운 계산 ㅠㅠ");
@@ -6,11 +6,22 @@ const hardCalculate = (number)  => {
   return number + 10000;
 }
 
+const easyCalculate = (number)  => {
+  console.log("쉬운 계산 ㅎㅎ");
+  return number + 1;
+}
+
 function App() {
 
   const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
 
-  const hardSum = hardCalculate(hardNumber);
+  // hard든 easy든 재렌더링 되면 둘 다 실행됨
+  // const hardSum = hardCalculate(hardNumber);
+  const hardSum = useMemo(() => {
+    return hardCalculate(hardNumber)
+  }, [hardNumber])  // 의존성 배열
+  const easySum = easyCalculate(easyNumber);
 
   return (
     <div>
@@ -21,6 +32,15 @@ function App() {
         onChange={(e) => setHardNumber(parseInt(e.target.value))}
       />
       <span> + 10000 = {hardSum}</span>
+
+
+      <h3>쉬운 계산기</h3>
+      <input
+        type="number"
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
     </div>
   );
 }
