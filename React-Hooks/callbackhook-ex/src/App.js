d10ffe8b4ed50676c9a9bdb289fa199df25bc09b
ssoyeong <jsy171771@gmail.com>
2022-09-13 22:28:15 +0900

    React-Hooks: useCallback 사각형 예제

--- React-Hooks/callbackhook-ex/src/App.js
+++ React-Hooks/callbackhook-ex/src/App.js
@@ -1,29 +1,29 @@
 import { useEffect, useState, useCallback } from 'react';
+import Box from './Box';
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [toggle, setToggle] = useState(true);
+  const [size, setSize] = useState(100);
+  const [isDark, setIsDark] = useState(false);
 
-  const someFunction = useCallback(() => {
-    console.log(`someFunc: number: ${number}`);
-    return;
-  }, [number]);   // 처음 렌더링될 때만 함수 호출. 그 이후는 메모이제이션 된 해당 함수 사용.
-
-
-  useEffect(() => {
-    console.log("someFunction changed");
-  }, [someFunction]);   // someFunction 객체. 주소값이 다름. 렌더링될 때마다 someFunction 호출됨.
+  const createBoxStyle = useCallback(() => {
+    return {
+      backgroundColor: 'pink',
+      width: `${size}px`,
+      height: `${size}px`,
+    };
+  }, [size]);
 
   return (
-    <div>
+    <div style={{
+      background: isDark ? "black" : "white",
+    }}>
       <input
         type="number"
-        valye={number}
-        onChange={(e) => setNumber(e.target.value)}
+        valye={size}
+        onChange={(e) => setSize(e.target.value)}
       />
-      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
-      <br />
-      <button onClick={someFunction}>Call someFunc</button>
+      <button onClick={() => setIsDark(!isDark)}>Change Theme</button>
+      <Box createBoxStyle={createBoxStyle} />
     </div>
   );
 }
