95bcdf91fd380657f57271306f73672315aae815
ssoyeong <jsy171771@gmail.com>
2022-09-24 00:12:26 +0900

    Assets와 Metadata

--- my-first-next/pages/posts/mypage.js
+++ my-first-next/pages/posts/mypage.js
@@ -1,11 +1,23 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function Mypage() {
     return (
         <>
-            <h2>
-                Click: <Link href="/">Back to home</Link>
-            </h2>
+            <Head>
+                <title>전소영님의 페이지</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="laxyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, ...`)
+                    }
+                />
+            </Head>
+            <h4>
+                <Link href="/">Back to home</Link>
+            </h4>
             <h1>This my Mypage</h1>
         </>
     );
