bf1387189672de561ccd325858c70d0fefea0e64
ssoyeong <jsy171771@gmail.com>
2022-09-24 11:27:26 +0900

    Feat: 공지사항 페이지 추가 Static generation of Pre-rendering

--- my-first-next/pages/index.js
+++ my-first-next/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import SupportMenu from '../components/supportMenu';
 
 export default function Home() {
   return (
-    <div>
-      <header>
+    <div> 
+      <Head>
         <Image
           priority
           src="/../public/images/header.jpg"
@@ -13,11 +14,26 @@ export default function Home() {
           width={1680}
           alt=""
         />
-      </header>
-      <h1>Form Duo 홈 화면입니다.</h1>
-      <h4 className='title'>
-        <Link href="/posts/mypage">마이페이지로 가기</Link>
-      </h4>
+      </Head>
+
+      <main>
+        <h1>Form Duo 홈 화면입니다.</h1>
+        <h4 className='title'>
+          <Link href="/posts/mypage">마이페이지로 가기</Link>
+        </h4>
+        <h4 className='title'>
+          <Link href="/posts/support/notice">공지사항으로 가기</Link>
+          <SupportMenu />
+        </h4>
+        <div>
+          폼듀는 클라우드 기반 설문 조사 서비스를 제공합니다.
+        </div>
+      </main>
+
+      <footer>
+
+      </footer>
+     
     </div>
   )
 }
---
+++ my-first-next/pages/posts/support/notice.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import utilStyles from '../../../styles/utils.module.css';
+import { getSortedPostsData } from '../../../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Notice({ allPostsData }) {
+    return (
+        <div>
+            <Head>
+                <title>공지사항</title>
+            </Head>
+            <h4>
+                <Link href="/">홈으로 돌아가기</Link>
+            </h4>
+            <h1>공지사항 페이지입니다.</h1>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Notice</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </div>
+    );
+}
