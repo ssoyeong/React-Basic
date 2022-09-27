bdc2e487ff7249968cab76dc40366999812a9e8a
ssoyeong <jsy171771@gmail.com>
2022-09-27 19:43:58 +0900

    Feat: pagination 버튼 정렬

--- my-first-next/components/support/supportContent.js
+++ my-first-next/components/support/supportContent.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import Search from './search';
 import utilStyles from '../../styles/utils.module.css';
 import Date from '../../components/date';
-import PaginationBtn from '../../components/paginationBtn';
+import PaginationBtn from './paginationBtn';
 import Link from 'next/link';
+import { Grid } from '@mui/material';
 
 export default function SupportContent({ pageName, allPostsData }) {
 
@@ -14,24 +15,32 @@ export default function SupportContent({ pageName, allPostsData }) {
                 <Search />
             </div>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                    <h2 className={utilStyles.headingLg}>전체 {pageName}</h2>
-                    <ul className={utilStyles.list}>
-                        {allPostsData.map(({ id, date, title }) => (
-                            <li className={utilStyles.listItem} key={id}>
-                                <Link href={`/posts/support/${pageName}/${id}`}>
-                                    <a>{title}</a>
-                                </Link>
-                                <br />
-                                <small className={utilStyles.lightText}>
-                                    <Date dateString={date} />
-                                </small>
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            <div>
-                <PaginationBtn />
-            </div>
+                <h2 className={utilStyles.headingLg}>전체 {pageName}</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/support/${pageName}/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid item xs={3}>
+                    <PaginationBtn />
+                </Grid>
+            </Grid>
         </div>
     );
 }
